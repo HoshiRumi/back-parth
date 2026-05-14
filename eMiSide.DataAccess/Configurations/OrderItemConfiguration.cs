2176d86a2397e973cad5487105629d3b1accbad6
HoshiRumi <dimabelyh86@gmail.com>
2026-05-14 03:58:13 +0300

    Add  OrderItemConfiguration.cs and some changes

---
+++ eMiSide.DataAccess/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using eDomain.mEntities.mOrder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eMiSide.DataAccess.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItemData>
+    {
+        public void Configure(EntityTypeBuilder<OrderItemData> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.UnitPrice).HasColumnType("decimal(18,2)");
+        }
+    }
+}
